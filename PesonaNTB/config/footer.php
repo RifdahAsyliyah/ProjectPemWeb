8e71b5b1b10fd001216a6b786303386afbb4aee3
RifdahAsyliyah <rasyliyah@gmail.com>
2026-06-15 00:02:46 +0800

    revisi footer

--- PesonaNTB/config/footer.php
+++ PesonaNTB/config/footer.php
@@ -5,32 +5,36 @@
         <div class="footer-brand">Pesona<span>NTB</span></div>
         <p>Platform informasi wisata Nusa Tenggara Barat yang menyajikan destinasi lengkap di Lombok dan Sumbawa.</p>
       </div>
+
       <div class="footer-col">
         <h4>Navigasi</h4>
         <ul>
-          <li><a href="../index.php">Beranda</a></li>
-          <li><a href="destinasi.php">Destinasi</a></li>
-          <li><a href="destinasi.php?kategori=all">Kategori</a></li>
-          <li><a href="../index.php#tentang">Tentang</a></li>
+          <li><a href="/ProjectPemWeb/ProjectPemWeb/PesonaNTB/index.php">Beranda</a></li>
+          <li><a href="/ProjectPemWeb/ProjectPemWeb/PesonaNTB/config/destinasi.php">Destinasi</a></li>
+          <li><a href="/ProjectPemWeb/ProjectPemWeb/PesonaNTB/index.php#kategori">Kategori</a></li>
+          <li><a href="/ProjectPemWeb/ProjectPemWeb/PesonaNTB/index.php#tentang">Tentang</a></li>
         </ul>
       </div>
+
       <div class="footer-col">
         <h4>Akun</h4>
         <ul>
-          <li><a href="login.php">Masuk</a></li>
-          <li><a href="register.php">Daftar</a></li>
+          <li><a href="/ProjectPemWeb/ProjectPemWeb/PesonaNTB/config/login.php">Masuk</a></li>
+          <li><a href="/ProjectPemWeb/ProjectPemWeb/PesonaNTB/config/register.php">Daftar</a></li>
         </ul>
       </div>
+
       <div class="footer-col">
         <h4>Kategori Wisata</h4>
         <ul>
-          <li><a href="destinasi.php?kategori=pantai">Pantai</a></li>
-          <li><a href="destinasi.php?kategori=gunung">Gunung</a></li>
-          <li><a href="destinasi.php?kategori=pulau">Pulau</a></li>
-          <li><a href="destinasi.php?kategori=budaya">Budaya</a></li>
+          <li><a href="/ProjectPemWeb/ProjectPemWeb/PesonaNTB/config/destinasi.php?kategori=pantai">Pantai</a></li>
+          <li><a href="/ProjectPemWeb/ProjectPemWeb/PesonaNTB/config/destinasi.php?kategori=gunung">Gunung</a></li>
+          <li><a href="/ProjectPemWeb/ProjectPemWeb/PesonaNTB/config/destinasi.php?kategori=pulau">Pulau</a></li>
+          <li><a href="/ProjectPemWeb/ProjectPemWeb/PesonaNTB/config/destinasi.php?kategori=budaya">Budaya</a></li>
         </ul>
       </div>
     </div>
+
     <div class="footer-bottom">
       <span>&copy; <?= date('Y') ?> PesonaNTB &mdash; Universitas Mataram</span>
       <span>Teknik Informatika &middot; Fakultas Teknik</span>
