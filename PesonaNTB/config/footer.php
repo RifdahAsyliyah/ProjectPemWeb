ba10463e2f462f5a6c052c8d0d1810c6953731a4
melaniputrizahari <melaniputrizahari@gmail.com>
2026-06-16 20:18:35 +0800

    footerrev

--- PesonaNTB/config/footer.php
+++ PesonaNTB/config/footer.php
@@ -1,38 +1,40 @@
 <footer class="footer">
   <div class="footer-inner">
     <div class="footer-top">
+
       <div class="footer-about">
         <div class="footer-brand">Pesona<span>NTB</span></div>
         <p>Platform informasi wisata Nusa Tenggara Barat yang menyajikan destinasi lengkap di Lombok dan Sumbawa.</p>
       </div>
 
       <div class="footer-col">
-        <h4>Navigasi</h4>
+        <h4>Kontak</h4>
         <ul>
-          <li><a href="/ProjectPemWeb/ProjectPemWeb/PesonaNTB/index.php">Beranda</a></li>
-          <li><a href="/ProjectPemWeb/ProjectPemWeb/PesonaNTB/config/destinasi.php">Destinasi</a></li>
-          <li><a href="/ProjectPemWeb/ProjectPemWeb/PesonaNTB/index.php#kategori">Kategori</a></li>
-          <li><a href="/ProjectPemWeb/ProjectPemWeb/PesonaNTB/index.php#tentang">Tentang</a></li>
+          <li><a href="https://mail.google.com/mail/?view=cm&fs=1&to=admin@pesonantb.com" target="_blank">admin@pesonantb.com</a></li>
+          <li><a href="https://wa.me/6285338551679" target="_blank">+62 85-338-551-679</a></li>
+          <li><a href="https://maps.apple.com/place?place-id=I745C25CB192613CB&address=Jl.+Majapahit+No+62%2C+Gomong%2C+Selaparang%2C+Mataram%2C+Nusa+Tenggara+Barat+83115%2C+Indonesia&coordinate=-8.588026%2C116.092948&name=Universitas+Mataram&_provider=9902" target="_blank">Universitas Mataram, Lombok</a></li>
         </ul>
       </div>
 
       <div class="footer-col">
-        <h4>Akun</h4>
-        <ul>
-          <li><a href="/ProjectPemWeb/ProjectPemWeb/PesonaNTB/config/login.php">Masuk</a></li>
-          <li><a href="/ProjectPemWeb/ProjectPemWeb/PesonaNTB/config/register.php">Daftar</a></li>
-        </ul>
+        <h4>Ikuti Kami</h4>
+        <div class="sosmed-links">
+          <a href="https://www.instagram.com/officialunram/?hl=en" target="_blank" aria-label="Instagram">
+            <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+              <rect x="2" y="2" width="20" height="20" rx="5" ry="5"/><circle cx="12" cy="12" r="4"/>
+              <circle cx="17.5" cy="6.5" r="0.5" fill="currentColor" stroke="none"/>
+            </svg>
+            <span>Instagram</span>
+          </a>
+          <a href="https://www.tiktok.com/@officialunram" target="_blank" aria-label="TikTok">
+            <svg width="14" height="14" viewBox="0 0 24 24" fill="currentColor">
+              <path d="M19.59 6.69a4.83 4.83 0 0 1-3.77-4.25V2h-3.45v13.67a2.89 2.89 0 0 1-2.88 2.5 2.89 2.89 0 0 1-2.89-2.89 2.89 2.89 0 0 1 2.89-2.89c.28 0 .54.04.79.1V9.01a6.33 6.33 0 0 0-.79-.05 6.34 6.34 0 0 0-6.34 6.34 6.34 6.34 0 0 0 6.34 6.34 6.34 6.34 0 0 0 6.33-6.34V8.69a8.27 8.27 0 0 0 4.83 1.54V6.78a4.85 4.85 0 0 1-1.06-.09z"/>
+            </svg>
+            <span>TikTok</span>
+          </a>
+        </div>
       </div>
 
-      <div class="footer-col">
-        <h4>Kategori Wisata</h4>
-        <ul>
-          <li><a href="/ProjectPemWeb/ProjectPemWeb/PesonaNTB/config/destinasi.php?kategori=pantai">Pantai</a></li>
-          <li><a href="/ProjectPemWeb/ProjectPemWeb/PesonaNTB/config/destinasi.php?kategori=gunung">Gunung</a></li>
-          <li><a href="/ProjectPemWeb/ProjectPemWeb/PesonaNTB/config/destinasi.php?kategori=pulau">Pulau</a></li>
-          <li><a href="/ProjectPemWeb/ProjectPemWeb/PesonaNTB/config/destinasi.php?kategori=budaya">Budaya</a></li>
-        </ul>
-      </div>
     </div>
 
     <div class="footer-bottom">
