898c567f2be454675cb2cd2197dcae9c59a16f13
melaniputrizahari <melaniputrizahari@gmail.com>
2026-06-16 20:24:43 +0800

    footerrev

--- PesonaNTB/config/footer.php
+++ PesonaNTB/config/footer.php
@@ -10,23 +10,48 @@
       <div class="footer-col">
         <h4>Kontak</h4>
         <ul>
-          <li><a href="https://mail.google.com/mail/?view=cm&fs=1&to=admin@pesonantb.com" target="_blank">admin@pesonantb.com</a></li>
-          <li><a href="https://wa.me/6285338551679" target="_blank">+62 85-338-551-679</a></li>
-          <li><a href="https://maps.apple.com/place?place-id=I745C25CB192613CB&address=Jl.+Majapahit+No+62%2C+Gomong%2C+Selaparang%2C+Mataram%2C+Nusa+Tenggara+Barat+83115%2C+Indonesia&coordinate=-8.588026%2C116.092948&name=Universitas+Mataram&_provider=9902" target="_blank">Universitas Mataram, Lombok</a></li>
+          <li>
+            <a href="https://mail.google.com/mail/?view=cm&fs=1&to=admin@pesonantb.com" target="_blank">
+              <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
+                <polyline points="22,6 12,13 2,6"></polyline>
+              </svg>
+              <span>admin@pesonantb.com</span>
+            </a>
+          </li>
+          
+          <li>
+            <a href="https://wa.me/6285338551679" target="_blank">
+              <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
+              </svg>
+              <span>+62 85-338-551-679</span>
+            </a>
+          </li>
+          
+          <li>
+            <a href="https://maps.apple.com/place?place-id=I745C25CB192613CB&address=Jl.+Majapahit+No+62%2C+Gomong%2C+Selaparang%2C+Mataram%2C+Nusa+Tenggara+Barat+83115%2C+Indonesia&coordinate=-8.588026%2C116.092948&name=Universitas+Mataram&_provider=9902" target="_blank">
+              <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
+                <circle cx="12" cy="10" r="3"></circle>
+              </svg>
+              <span>Universitas Mataram, Lombok</span>
+            </a>
+          </li>
         </ul>
       </div>
 
       <div class="footer-col">
         <h4>Ikuti Kami</h4>
         <div class="sosmed-links">
-          <a href="https://www.instagram.com/officialunram/?hl=en" target="_blank" aria-label="Instagram">
+          <a href="https://instagram.com" target="_blank" aria-label="Instagram">
             <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
               <rect x="2" y="2" width="20" height="20" rx="5" ry="5"/><circle cx="12" cy="12" r="4"/>
               <circle cx="17.5" cy="6.5" r="0.5" fill="currentColor" stroke="none"/>
             </svg>
             <span>Instagram</span>
           </a>
-          <a href="https://www.tiktok.com/@officialunram" target="_blank" aria-label="TikTok">
+          <a href="https://tiktok.com" target="_blank" aria-label="TikTok">
             <svg width="14" height="14" viewBox="0 0 24 24" fill="currentColor">
               <path d="M19.59 6.69a4.83 4.83 0 0 1-3.77-4.25V2h-3.45v13.67a2.89 2.89 0 0 1-2.88 2.5 2.89 2.89 0 0 1-2.89-2.89 2.89 2.89 0 0 1 2.89-2.89c.28 0 .54.04.79.1V9.01a6.33 6.33 0 0 0-.79-.05 6.34 6.34 0 0 0-6.34 6.34 6.34 6.34 0 0 0 6.34 6.34 6.34 6.34 0 0 0 6.33-6.34V8.69a8.27 8.27 0 0 0 4.83 1.54V6.78a4.85 4.85 0 0 1-1.06-.09z"/>
             </svg>
